87edf7be013fdfb9e735934e480428531f0c9beb
dmonsalud <douglas.monsalud@mlb.com>
2022-11-27 19:59:06 -0700

    feat(prefs): save weather result to sharedprefs

--- app/src/main/java/com/dmonsalud/weatherapp/ui/WeatherListFragment.kt
+++ app/src/main/java/com/dmonsalud/weatherapp/ui/WeatherListFragment.kt
@@ -2,6 +2,7 @@ package com.dmonsalud.weatherapp.ui
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.SharedPreferences
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -18,6 +19,7 @@ import com.google.gson.Gson
 class WeatherListFragment : Fragment() {
 
     private lateinit var binding: FragmentListWeatherBinding
+    private var sharedPreferences: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,14 +51,25 @@ class WeatherListFragment : Fragment() {
         val networkStatusChecker = NetworkStatusChecker(connectivityManager)
 
         if (networkStatusChecker.hasInternetConnection()) {
+            sharedPreferences = activity?.getSharedPreferences("weatherPreferences", Context.MODE_PRIVATE)
+            val editor = sharedPreferences?.edit()
+
             val weatherJsonStringHolder = HttpRequest(ZIPCODE).execute().get()
+            editor?.putString("five_day_weather_result", weatherJsonStringHolder)
+            editor?.apply()
             val fiveDayWeatherResult = Gson().fromJson(weatherJsonStringHolder, FiveDayWeatherResult::class.java)
             binding.rvWeatherList.adapter = WeatherListAdapter(fiveDayWeatherResult)
         } else {
-            AlertDialog.Builder(activity).setTitle("No Internet Connection")
-                .setMessage("Please check your internet connection and try again")
-                .setPositiveButton(android.R.string.ok) { _, _ -> }
-                .setIcon(android.R.drawable.ic_dialog_alert).show()
+            sharedPreferences?.let {
+                val jsonStringFromPrefs = sharedPreferences?.getString("five_day_weather_result", "N/A")
+                val fiveDayWeatherResultFromPrefs = Gson().fromJson(jsonStringFromPrefs, FiveDayWeatherResult::class.java)
+                binding.rvWeatherList.adapter = WeatherListAdapter(fiveDayWeatherResultFromPrefs)
+            } ?: run {
+                AlertDialog.Builder(activity).setTitle("No Internet Connection")
+                    .setMessage("Please check your internet connection and try again")
+                    .setPositiveButton(android.R.string.ok) { _, _ -> }
+                    .setIcon(android.R.drawable.ic_dialog_alert).show()
+            }
         }
     }
 
